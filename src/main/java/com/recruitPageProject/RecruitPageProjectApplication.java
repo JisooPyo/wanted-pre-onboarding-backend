a2d998e8eecf277cb107922e64b2aa8745daa683
JisooPyo <audwl326@gmail.com>
2023-10-05 16:35:38 +0900

    init: first commit

---
+++ src/main/java/com/recruitPageProject/RecruitPageProjectApplication.java
@@ -0,0 +1,13 @@
+package com.recruitPageProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitPageProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitPageProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recruitPageProject/RecruitPageProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recruitPageProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitPageProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
