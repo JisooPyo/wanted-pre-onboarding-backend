925af4c6890049680f00e9cbdfa4065efa253982
JisooPyo <audwl326@gmail.com>
2023-10-06 20:08:05 +0900

    feature: 채용 공고 수정 API (#12)

--- src/main/java/com/recruitPageProject/common/exception/CustomErrorCode.java
+++ src/main/java/com/recruitPageProject/common/exception/CustomErrorCode.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum CustomErrorCode {
 	//	USER_NOT_FOUND(HttpStatus.BAD_REQUEST.value(), "존재하지 않는 사용자입니다."),
-	COMPANY_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "존재하지 않는 회사입니다.");
+	COMPANY_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "존재하지 않는 회사입니다."),
+	JOBPOST_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "존재하지 않는 채용공고입니다."),
+	NO_AUTHORIZATION(HttpStatus.UNAUTHORIZED.value(), "권한이 없습니다.");
 
 	private final int errorCode;
 	private final String errorMessage;
