e09cb1828fb848d589df3523a25c6730ce7b15d7
JisooPyo <audwl326@gmail.com>
2023-10-07 01:47:15 +0900

    complete: 채용공고 등록 API Test 구현

--- src/main/java/com/recruitPageProject/jobPost/dto/JobPostRequestDto.java
+++ src/main/java/com/recruitPageProject/jobPost/dto/JobPostRequestDto.java
@@ -2,9 +2,11 @@ package com.recruitPageProject.jobPost.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class JobPostRequestDto {
 	@NotNull
 	Long companyId;
