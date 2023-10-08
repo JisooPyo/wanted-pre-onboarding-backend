94a63da1dc57ac176411302c5ad42a85d9be42fe
JisooPyo <audwl326@gmail.com>
2023-10-08 20:50:58 +0900

    test: 채용공고 삭제 API unit test (#36)
    * update: 테스트 코드 위해서 `@Builder` 추가
    
    * complete: 채용공고 삭제 API unit test
    
    * chore: 필요없는 import 제거

--- src/main/java/com/recruitPageProject/jobPost/dto/JobPostDeleteRequestDto.java
+++ src/main/java/com/recruitPageProject/jobPost/dto/JobPostDeleteRequestDto.java
@@ -1,9 +1,11 @@
 package com.recruitPageProject.jobPost.dto;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class JobPostDeleteRequestDto {
 	@NotNull
 	Long companyId;
