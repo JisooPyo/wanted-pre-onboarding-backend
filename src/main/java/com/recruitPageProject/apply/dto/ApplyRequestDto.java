6f99a72f2ba3e5bb239995716ab7a23c456d7c09
JisooPyo <audwl326@gmail.com>
2023-10-08 22:04:38 +0900

    update: 테스트 코드 위한 `@Builder` 어노테이션 추가

--- src/main/java/com/recruitPageProject/apply/dto/ApplyRequestDto.java
+++ src/main/java/com/recruitPageProject/apply/dto/ApplyRequestDto.java
@@ -1,9 +1,11 @@
 package com.recruitPageProject.apply.dto;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class ApplyRequestDto {
 	@NotNull
 	Long jobPostId;
